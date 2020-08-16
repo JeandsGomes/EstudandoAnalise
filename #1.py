5effcd091fe3979990024f45dae927d8e7c0228f
Jeanderson Gomes <jenandersondesousagomes@gmail.com>
2020-08-16 08:52:58 -0300

    16-ago

---
+++ #1.py
@@ -0,0 +1,33 @@
+#ANOTAÇÕES DE LEITURA DA APOSTILA#
+#Pandas, NumPy e IPython
+
+
+"""
+COLETA E PREPROCESSAMENTO:
+ > Capitar
+ > Organizar
+ > Tratar os dados
+
+Entre as principais funções podemos
+destacar:
+
+1 - Seleção ou Redução : Apos o dominio do problema é nescessario uma analize e seleção das informações
+que são consideradas relevantes
+
+2 - Limpeza de dados : Cuidando de dados inconsistentes
+
+3 - Codificação de dados : Formatar os dados para que eles possão funcionar como entrada do algoritmo
+
+4 - Enriquecimento de dados : Função responsavel por obter o maximo de informações possiveis que podem ser
+obitidos por meio dos dados ja existentes
+
+
+MINERAÇÃO DE DADOS:
+
+(???)
+ > Modelos
+ > Criterio de Preferencia
+ > Sumariozação
+(???)
+
+"""
